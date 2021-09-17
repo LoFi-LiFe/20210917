1ad18ae0c97c82a5a812845bef503e53fada9d32
LoFi-LiFe <newpmt88@gmail.com>
2021-09-17 13:25:45 +0900

    두수의 합

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	int MyCash = 1000;
-	float HP = 100.0f;
-	char H = '\n';
+	int First = 0;
+	int Second = 0;
+	int Sum = 0;
+	
+	cin >> First;  // 첫번째 숫자를 입력
+	cin >> Second; // 두번째 숫자를 입력
 
-	std::cout << "aaa" << H;
+	Sum = First + Second; //두 정수를 합한것이 Sum
+	
+	cout << Sum; //Sum을 출력한다
 
 	return 0;
 }
